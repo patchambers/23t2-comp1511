39682fbc3cadac9c4e0f4a758d82cfc0cbc3b673
Pat Chambers <z5264081@cse.unsw.edu.au>
2023-05-30 16:47:12 +1000

    Added code files

---
+++ week01/face.c
@@ -0,0 +1,14 @@
+// Changing hello_world.c to print out a face instead
+// Pat Chambers (z5264081), 30/5/23
+
+#include <stdio.h>
+
+int main (void) {
+
+    printf("~ ~\n");
+    printf("0 0\n");
+    printf(" o \n");
+    printf("\\_/\n");
+    
+    return 0;
+}  
---
+++ week01/hello_world.c
@@ -0,0 +1,11 @@
+// Basic Hello World program
+// Marc Chee, September 2020
+
+#include <stdio.h>
+
+int main (void) {
+
+    printf("Hello World\n");
+    
+    return 0;
+}  
